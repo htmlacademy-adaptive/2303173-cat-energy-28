fc5a6d274383ea6d78fff5b61779a6638bcfc7e3
PeachyF <kseni.hoeny@mail.ru>
2023-05-13 04:06:55 +0300

    OMG!!! COMPARSION SLIDER IS WORKING

--- source/js/slider.js
+++ source/js/slider.js
@@ -3,10 +3,17 @@ const dragLine = document.querySelector(".slider .drag-line");
 const img = document.querySelector(".images .img-2");
 const imgFirst = document.querySelector(".images .img-1");
 
-slider.oninput = ()=>{
+const mySuperFunc = ()=>{
     let sliderVal = slider.value;
     dragLine.style.left = sliderVal + "%";
     img.style.width = sliderVal + "%";
-    // imgFirst.style.marginRight = 100 - sliderVal + "%";
+    imgFirst.style.width = 100 - sliderVal + "%";
 }
 
+slider.oninput = mySuperFunc
+mySuperFunc()
+
+// setInterval(() => {
+//     const color = document.body.style.backgroundColor;
+//     document.body.style.backgroundColor = color === 'red' ? 'transparent' : 'red'
+//   }, 100);
