74d85e2b3eee1652dddd97e7463c5b74293524a6
PeachyF <kseni.hoeny@mail.ru>
2023-05-14 16:31:45 +0300

    Update comparsion slider

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,7 +1,7 @@
-const slider = document.querySelector(".slider input");
-const dragLine = document.querySelector(".slider .drag-line");
-const img = document.querySelector(".images .img-2");
-const imgFirst = document.querySelector(".images .img-1");
+const slider = document.querySelector(".compare-slider__slider input");
+const dragLine = document.querySelector(".compare-slider__slider .compare-slider__drag-line");
+const img = document.querySelector(".compare-slider__images .compare-slider__image-second");
+const imgFirst = document.querySelector(".compare-slider__images .compare-slider__image-first");
 
 const mySuperFunc = ()=>{
     let sliderVal = slider.value;
